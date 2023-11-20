eea97a5bb6f0bd3c9b119e7bc26b0e4bf608265e
Armaan Mishra <armaanmishra48@gmail.com>
2023-11-21 04:51:31 +0530

    add landing page and navbar

---
+++ src/components/NavItems.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import { PRODUCT_CATEGORIES } from "@/config";
+import { useRef, useState, useEffect } from "react";
+import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/use-on-click-outiside";
+
+const NavItems = () => {
+    const [activeIndex, setActiveIndex] = useState<null | number>(null);
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+          if (e.key === 'Escape') {
+            setActiveIndex(null)
+          }
+        }
+    
+        document.addEventListener('keydown', handler)
+    
+        return () => {
+          document.removeEventListener('keydown', handler)
+        }
+      }, [])
+    const isAnyOpen = activeIndex !== null;
+
+    const navRef = useRef<HTMLDivElement | null>(null);
+    useOnClickOutside(navRef, () => setActiveIndex(null));
+
+    return <div ref={navRef} className="flex gap-4 h-full">
+        {PRODUCT_CATEGORIES.map((category, i) => {
+            const handleOpen = () => {
+                if (activeIndex === i) {
+                    setActiveIndex(null);
+                } else {
+                    setActiveIndex(i);
+                }
+            }
+            const close = () => setActiveIndex(null);
+            const isOpen = i === activeIndex;
+            return (<NavItem close={close} category={category} handleOpen={handleOpen} isOpen={isOpen} key={category.value} isAnyOpen={isAnyOpen} />)
+        })}
+    </div>
+};
+
+export default NavItems;
