0b9f0618bc3706928e56f9498b556d04f8c1e379
Armaan Mishra <armaanmishra48@gmail.com>
2023-11-26 03:31:17 +0530

    finish sign in feature

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -12,36 +12,49 @@ import { AuthCredentialsValidator, TAuthCredentialsValidator } from "@/lib/valid
 import { trpc } from "@/trpc/client";
 import { toast } from 'sonner';
 import { ZodError } from "zod";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const Page = () => {
+
+  const searchParams = useSearchParams();
+  const isSeller = searchParams.get('as') === 'seller';
+  const origin = searchParams.get('origin');
+
+  const continueAsSeller = () => {
+    router.push("?as=seller");
+  }
+  const continueAsBuyer = () => {
+    router.replace('/sign-in', undefined);
+  }
 
     const router = useRouter();
     const { register, handleSubmit, formState: { errors } } = useForm<TAuthCredentialsValidator>({
         resolver: zodResolver(AuthCredentialsValidator),
     });
 
-    const { mutate, isLoading } = trpc.auth.createPayloadUser.useMutation({
-      onError: (err) => {
-        if (err.data?.code === 'CONFLICT') {
-          toast.error("This email is already in use. Sign in instead")
-          return
-        }
-        if (err instanceof ZodError) {
-          toast.error(err.issues[0].message)
-          return
-        }
-        toast.error('Something went wrong. Please try again.')
-      },
-
-      onSuccess: ({ sentToEmail }) => {
-        toast.success(`Verification email sent to ${sentToEmail}`);
-        router.push('/verify-email?to=' + sentToEmail);
-      },
-    })
+  const { mutate: singIn, isLoading } = trpc.auth.signIn.useMutation({
+    onSuccess: () => {
+      toast.success("Signed in successfully.");
+      router.refresh();
+      if (origin) {
+        router.push(`/${origin}`);
+        return;
+      }
+      if (isSeller) {
+        router.push('/sell');
+        return;
+      }
+      router.push('/');
+    },
+    onError: (err) => {
+      if (err.data?.code === 'UNAUTHORIZED') {
+        toast.error('Invalid email or password');
+      }
+    },
+  });
     
     const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {
-        mutate({ email, password });
+        singIn({ email, password });
   };
   
 
@@ -52,7 +65,7 @@ const Page = () => {
             <div className='flex flex-col items-center space-y-2 text-center'>
               <Icons.logo className='h-20 w-20' />
               <h1 className='text-2xl font-semibold tracking-tight'>
-                Sign in
+                Sign in into your {isSeller ? "seller" : ""} account
               </h1>
   
               <Link
@@ -120,7 +133,11 @@ const Page = () => {
               </div>
             </div>
               
-                        
+              {isSeller ? (
+              <Button onClick={continueAsBuyer} variant={'secondary'} disabled={isLoading}>Continue as customer</Button>
+              ) : (
+              <Button onClick={continueAsSeller} variant={'secondary'} disabled={isLoading}>Continue as seller</Button>
+              ) }         
 
             </div>
           </div>
