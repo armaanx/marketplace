1bf90bb9aca89789a33b00fd64c18c5121ab0391
Armaan Mishra <armaanmishra48@gmail.com>
2023-12-01 11:43:06 +0530

    add product detail page

---
+++ src/components/AddToCartButton.tsx
@@ -0,0 +1,22 @@
+'use client'
+import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+
+const AddToCartButton = () => {
+    const [isSuccess, setIsSuccess] = useState(false);
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setIsSuccess(false)
+        }, 2000);
+        return () => clearTimeout(timeout);
+    }, [isSuccess]);
+    return (
+        <Button onClick={() => {
+            setIsSuccess(true);
+        }} size={'lg'} className="w-full">
+            {isSuccess ? 'Added!' : 'Add to Cart'}
+        </Button>
+    );
+};
+
+export default AddToCartButton;
